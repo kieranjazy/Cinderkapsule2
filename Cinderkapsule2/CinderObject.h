0ec7003b3def42def458765e360014a46b7e42d4
Kieran Jayes <kieranjayes@hotmail.com>
2022-01-17 00:14:54 +0000

    Delete Cinderkapsule2 directory

--- Cinderkapsule2/CinderObject.h
+++
@@ -1,7 +0,0 @@
-namespace Cinder {
-	class CinderObject {
-		const virtual void tick() = 0;
-	};
-}
-
-using namespace Cinder; //Namespaces are just for organisation; I don't mind using statements for my own namespaces
--- Cinderkapsule2/ModelManager.cpp
+++
@@ -1,5 +0,0 @@
-#include "ModelManager.h"
-
-
-
-
